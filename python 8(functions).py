fee54f2c5acc1272bd40b1e29ff2959b86c83151
hariof98 <harisridhar77@gmail.com>
2021-05-20 19:29:47 +0530

    Second commit

--- python 8(functions).py
+++ python 8(functions).py
@@ -1,4 +1,4 @@
-# FUNCTIONS
+# #FUNCTIONS
 # def hello_python():
 #     print('Hello world')
 #     print('How are you?')
@@ -9,7 +9,7 @@
 # print('Thank You!')
 
 
-# Functions with parameters
+# # Functions with parameters
 # def hello_world(name):   # name is the parameter
 #     print(f'Hello {name} ')
 #     print('Welcome aboard')
@@ -22,12 +22,27 @@
 # print('Thank You!')
 
 
-def hello_world(first_name, last_name):
-    print(f'Hello, {first_name} {last_name} ')
-    print('Welcome aboard')
-    print('Stay safe')
+# def hello_world(first_name, last_name):
+#     print(f'Hello, {first_name} {last_name} ')
+#     print(f'Hello, {last_name} {first_name} ')
+#     print('Welcome aboard')
+#     print('Stay safe')
+#
+#
+# print('Greetings')
+# hello_world("Hari", "Sridhar")  # these 2 arguments are called positional arguments
+# hello_world(last_name="Sridhar", first_name="Hari")  # this is called keyword argument (It is used to improve the readability of the code,especially if we want to declare multiple numeric values.
+#         ##"Always use positional arguments first and then keyword argument
+# print('Thank You')
 
-
-print('Greetings')
-hello_world("Hari", "Sridhar")  # these 2 arguments are called positional arguments
-print('Thank You!')
+# #FUNCTION USING RETURN STATEMENTS
+#      #By Default all functions in Python return None
+#        #If we have a function that calculates something we can use return statement
+#
+# def calc_number(number):
+#     return number * number
+#
+#
+# #print(calc_number(3))
+# answer = calc_number(int(input('Please enter a number to calculate: ')))
+# print(answer)
